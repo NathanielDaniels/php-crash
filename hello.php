d5b630da39a8b9334b76d08c42e7bd6c7109d7dc
Nathaniel Daniels <nathaniel.daniels@tenh.com>
2023-03-14 17:34:24 -0500

    learning PHP syntax

--- hello.php
+++ hello.php
@@ -1,3 +1,76 @@
 <?php
-  echo "Hello! test test test";
+  echo "Hello,";
+  echo htmlspecialchars($_GET["name"]);
+
+  // phpinfo()
+  // echo $_SERVER['HTTP_USER_AGENT'];
+  // echo $_GET['name'];
+  // echo $_SERVER['DOCUMENT_ROOT'];
 ?>
+<?php 
+  // print_r([1,2,3,4,5,6,7,8,9,10]) 
+  $time = date("H");
+  // echo $time;
+  if ($time < "10") {
+    echo "! Have a good morning!";
+  } elseif ($time < "20") {
+    echo "! Have a good day!";
+  } else {
+    echo "! Have a good night!";
+  }
+
+  echo $time < '10' ? 'Good Morning' : 'Good Night';
+
+  $numbers = [1,2,3,4,5,6,7,8,9,10];
+  // foreach ($numbers as $number) {
+  //   echo $number + 1 . "<br />";
+  // }
+
+  // $multiply = fn($x) => $x * 2;
+
+  // echo $multiply(2);
+
+  $fruits = ['apple', 'banana', 'orange', 'grape', 'mango'];
+
+  // get length of array
+  // echo count($fruits);
+  echo "<br />";
+  //search array
+  // echo array_search('apple', $fruits);
+
+  // add to array
+  // array_push($fruits, 'pear');
+  
+  // remove from array
+  // array_shift($fruits);
+  // split array
+  // print_r(array_chunk($fruits, count($fruits) % 2 == 0 ? 2 : 3));
+  // concatenate arrays
+  // print_r(array_merge($fruits, $numbers));
+  // print_r([...$fruits, ...$numbers]);
+
+  // combine arrays
+  // print_r(array_combine($fruits, $fruits2));
+
+  // flip array
+  // print_r(array_flip($fruits));
+
+  // create array with range of numbers
+  print_r(range(1, 20));
+
+  // map array
+  print_r(array_map(fn($x) => $x * 2, $numbers));
+
+  // filter array
+  print_r(array_filter($numbers, fn($x) => $x <= 10));
+  // foreach($fruits as $fruit) {
+  //   echo $fruit . "<br />";
+  // }
+  // for ($x = 0; $x <= 10; $x++) {
+  //   echo " <br> The number is: $x";
+  // }
+?>
+<?= "<br />" ?>
+<!-- This does the same as above but single line -->
+<?= "Hello, " . htmlspecialchars($_GET["name"]) . "!"; ?>
+
--- index.php
+++ index.php
@@ -4,11 +4,14 @@
     <title>Include</title>
 </head>
     <body>
+        <h1>
+            <?= "Test"; ?>
+        </h1>
         <?php
             // Path: index.php
 
             // create a toggle button with the name "Click Me" that changes to "Clicked"
-            echo '<button id="myButton" onclick="changeText()">Click Me</button>';
+            echo '<button id="myButton" type="submit" onclick="changeText()">Click Me</button>';
             
             // if the button is clicked
             // if(isset($_POST['submit'])){
@@ -22,9 +25,9 @@
 
             // create a button with the name "submit"
             // and the value "submit"
-            // echo '<form action="/index.php" method="post">
-            // <input type="submit" name="submit" value="submit" />
-            // </form>';
+            echo '<form action="/index.php?name=Nathan" method="post">
+            <input type="submit" name="submit" value="submit" />
+            </form>';
 
             // // if the button is clicked
             // if(isset($_POST['submit'])){
@@ -32,14 +35,31 @@
             //     include 'hello.php';
             // }
             // echo "Hello! test does this is it working?";
+
+            // if button clicked, show hellp.php
+            if(isset($_POST['submit'])){
+                include 'hello.php';
+            }else{
+                echo "awaiting submit";
+            }
         ?>
         <!-- <button id="myButton" onclick="changeText()">Click Me</button> -->
         <script>
+
+                        // if(isset($_POST['submit'])){
+            //     // include the file "hello.php"
+            //     include 'hello.php';
+            // }
             // create a function that changes the button value to "Clicked"
+            console.time('test');
             function changeText(){
                 let button = document.getElementById("myButton");
                 button.innerHTML === "Click Me!" ? button.innerText = "Clicker" : button.innerText = "Click Me!";
             }
+            
+
+            // console.timeEnd('test end');
+            // console.trace(isDarkMode);
         </script>
     </body>
 </html>
